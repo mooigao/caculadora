38b0b19e6da9580f63658418ee45842a76023f1a
moigo <moigo@LAPTOP-8BM1EI2G>
2022-05-18 16:40:54 -0500

    Calcular

---
+++ src/main/java/Calculadora/Calculadora/Controllers/CalculadoraController.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package Calculadora.Calculadora.Controllers;
+
+/**
+ * @author moigo
+ *
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CalculadoraController {
+	
+	@GetMapping("/calculadora")
+	public String index(Model modelo) 
+	{
+		modelo.addAttribute("calcu", new calcular());
+		return "/calculadora/index";
+	}
+	
+	@PostMapping("/resultado")
+	
+	public String resultado(@ModelAttribute calcular calcu, Model modelo) 
+	{
+		calcu.Calcular();
+		modelo.addAttribute("calcu", calcu);
+		return "/calculadora/index";
+	}
+	
+}
